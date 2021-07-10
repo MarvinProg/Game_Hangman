c977d0400e7930ef97b00227f0be77f37506b45e
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-07-10 21:44:25 +0300

    Add body programm

---
+++ main.rb
@@ -0,0 +1,20 @@
+require_relative 'lib/console_interface'
+require_relative 'lib/game'
+
+puts "Всем привет!"
+
+# Загружаем случаное число из файла
+word = File.readlines(__dir__ + '/data/words.txt', encoding: 'UTF-8', chomp: true).sample
+game = Game.new(word)
+console_interface = ConsoleInterface.new(game)
+
+until game.over?
+  # Выводим состояние игры
+  console_interface.print_out
+  # Спрашиваем букву у пользователя
+  letter = console_interface.input
+  # Обновляем состояние игры
+  game.play!(letter)
+end
+
+console_interface.print_out
